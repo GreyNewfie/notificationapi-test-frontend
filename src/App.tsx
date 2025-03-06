3aed0d42d12cfcac79faed72900479e254cbd63c
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2025-03-06 16:44:28 -0330

    Initial commit with environment variables setup

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { NotificationAPIProvider, NotificationPopup } from '@notificationapi/react'
+import './App.css'
+
+function App() {
+  const userId = import.meta.env.VITE_USER_ID
+  const clientId = import.meta.env.VITE_CLIENT_ID
+
+  return (
+    <div className="app">
+      <NotificationAPIProvider
+        clientId={clientId}
+        userId={userId}
+      >
+        <h1>NotificationAPI Secure Mode Test</h1>
+        <div>
+          <h4>Here are your notifications!</h4>
+        </div>
+        <NotificationPopup />
+      </NotificationAPIProvider>
+    </div>
+  )
+}
+
+export default App
