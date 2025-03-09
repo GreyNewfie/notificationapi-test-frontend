2e309e8d9d62bd679a0da7f5bf64cc791bbb3f1d
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2025-03-09 13:28:45 -0230

    Remove unused useEffect import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { NotificationAPIProvider, NotificationPopup } from '@notificationapi/react'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 function NotificationControls() {
